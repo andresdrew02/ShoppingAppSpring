877c3485a6b890c183fa75d4b81199a90e4dd0f7
Andrés Del Cerro <alumno.254958@ies-azarquiel.es>
2023-09-10 20:30:07 +0200

    Deleted OLD files to work with the wrapper project

--- microservices-parent/MicroServices New/order-service/src/main/java/com/andres/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.andres.orderservice.repository;
-
-import com.andres.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- microservices-parent/MicroServices New/product-service/src/main/java/com/andres/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.andres.productservice.repository;
-
-import com.andres.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
