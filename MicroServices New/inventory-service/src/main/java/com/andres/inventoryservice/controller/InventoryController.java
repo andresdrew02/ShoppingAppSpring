7edfb1188cb3d19ee6764e23f9bffeb83e5eb69a
Andrés Del Cerro <alumno.254958@ies-azarquiel.es>
2023-09-11 18:03:41 +0200

    Update - InventoryService \ To check if a Product is in stock we have to pass multiple "skuCode" request param and returns a list of InventoryResponse to check multiple skuCodes in one get request

--- MicroServices New/inventory-service/src/main/java/com/andres/inventoryservice/controller/InventoryController.java
+++ MicroServices New/inventory-service/src/main/java/com/andres/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.andres.inventoryservice.controller;
 
+import com.andres.inventoryservice.dto.InventoryResponse;
 import com.andres.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,10 +15,9 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public Boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
-
 }
